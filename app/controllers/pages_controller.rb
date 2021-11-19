5fdddebf21895c5f270c918ff4db031aeb7dbfc7
Fernando R P Marques <frpm1001@gmail.com>
2021-11-19 20:00:44 -0300

    Change navbar desktop color, add exit button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
-  def qr_code_generator; end
-
-  def qr_code_result
-    @qrcode = RQRCode::QRCode.new(params[:content].to_s).as_svg(use_path: true, viewbox: true)
+  def qrcode
+    @url = "https://www.google.com"
+    @qrcode = RQRCode::QRCode.new(@url).as_svg(use_path: true, viewbox: true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   controller :pages do
-    get :qr_code_generator
-    get :qr_code_result
+    get :qrcode
   end
 end
