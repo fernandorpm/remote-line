919fae3484cfc5fae122b8234afb6524e56f9a05
Fernando R P Marques <frpm1001@gmail.com>
2021-11-19 00:01:41 -0300

    Add basic styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,6 @@ class PagesController < ApplicationController
   def qr_code_generator; end
 
   def qr_code_result
-    @qrcode = RQRCode::QRCode.new(params[:content].to_s).as_svg(
-      use_path: true,
-      viewbox: true
-    )
+    @qrcode = RQRCode::QRCode.new(params[:content].to_s).as_svg(use_path: true, viewbox: true)
   end
 end
